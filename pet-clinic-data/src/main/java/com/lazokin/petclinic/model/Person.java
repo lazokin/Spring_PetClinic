f9460149d4c1057015c5c8ee879b93788a845356
Nik Ambukovski <lazokin@hotmail.com>
2018-12-29 11:26:06 +1100

    implement base entity

--- pet-clinic-data/src/main/java/com/lazokin/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/lazokin/petclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.lazokin.petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 	
 	private String firstName;
 	
--- pet-clinic-data/src/main/java/com/lazokin/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/lazokin/petclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.lazokin.petclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
 
 	private PetType petType;
 	
