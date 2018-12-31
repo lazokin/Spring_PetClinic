a6662de99619bde783bcea2346f4f141db356631
Nik Ambukovski <lazokin@hotmail.com>
2018-12-31 11:26:42 +1100

    create vets page and controller

--- pet-clinic-web/src/main/java/com/lazokin/petclinic/controller/VetsController.java
+++ pet-clinic-web/src/main/java/com/lazokin/petclinic/controller/VetsController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class IndexController {
+public class VetsController {
 	
-	@RequestMapping({"", "/"})
+	@RequestMapping({"/vets"})
 	public String index() {
-		return "index";
+		return "vets/index";
 	}
 
 }
