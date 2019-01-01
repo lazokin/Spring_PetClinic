1dbc022f1f67c3fe2202804b51c7c9b95d2a2690
Nik Ambukovski <lazokin@hotmail.com>
2019-01-01 17:42:36 +1100

    list owners and vets

--- pet-clinic-web/src/main/java/com/lazokin/petclinic/controller/OwnersController.java
+++ pet-clinic-web/src/main/java/com/lazokin/petclinic/controller/OwnersController.java
@@ -1,14 +1,23 @@
 package com.lazokin.petclinic.controller;
 
+import com.lazokin.petclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnersController {
-	
+
+	private final OwnerService ownerService;
+
+	public OwnersController(OwnerService ownerService) {
+		this.ownerService = ownerService;
+	}
+
 	@RequestMapping({"", "/", "/index", "index.html"})
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("owners", ownerService.findAll());
 		return "owners/index";
 	}
 
