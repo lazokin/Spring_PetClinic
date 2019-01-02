5fcbb7b7ca5d611f9b72b218151a89dd21e79e73
Nik Ambukovski <lazokin@hotmail.com>
2019-01-02 15:39:47 +1100

    setup a vet specialties object

---
+++ pet-clinic-data/src/main/java/com/lazokin/petclinic/model/Speciality.java
@@ -0,0 +1,15 @@
+package com.lazokin.petclinic.model;
+
+public class Speciality extends BaseEntity {
+	
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
