65ca67e86aea58114ad9e6aa27e6180fdc7db487
Nik Ambukovski <lazokin@hotmail.com>
2019-01-03 16:59:00 +1100

    convert vets to jpa entities

--- pet-clinic-data/src/main/java/com/lazokin/petclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/com/lazokin/petclinic/model/Specialty.java
@@ -1,7 +1,14 @@
 package com.lazokin.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialty")
 public class Specialty extends BaseEntity {
 	
+	@Column(name = "name")
 	private String name;
 	
 	public String getName() {
